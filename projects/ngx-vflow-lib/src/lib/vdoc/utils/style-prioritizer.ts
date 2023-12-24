9980dd1d5957086bea936c64fc1523a3c2e97cc3
Artem Mangilev <artmnglv@gmail.com>
2023-12-24 11:39:36 +0600

    add comments for style prioritizer

--- projects/ngx-vflow-lib/src/lib/vdoc/utils/style-prioritizer.ts
+++ projects/ngx-vflow-lib/src/lib/vdoc/utils/style-prioritizer.ts
@@ -1,20 +1,18 @@
 import { ContainerStyleSheet } from "../interfaces/stylesheet.interface";
 
-// from lowest to highest
 export enum StylesSource {
   hover,
   focus,
   styleSheet,
 }
 
-type ElementStyleSheets = { [key in StylesSource]: ContainerStyleSheet | null }
-
 export class StylePrioritizer {
   private readonly _styleStateMachine = declareStyleStateMachine()
 
-  private readonly _elementStyles: ElementStyleSheets
+  private readonly _elementStyles: { [key in StylesSource]: ContainerStyleSheet | null }
 
   constructor(styleSheet: Required<ContainerStyleSheet>) {
+    // Extract sub-stylesheets from main stylesheet
     this._elementStyles = {
       [StylesSource.styleSheet]: styleSheet,
       [StylesSource.hover]: styleSheet.onHover,
@@ -22,26 +20,49 @@ export class StylePrioritizer {
     }
   }
 
+  /**
+   * Set stylesheet as active by source
+   *
+   * @param current
+   */
   public set(current: StylesSource) {
     this._styleStateMachine[current].isSet = true
   }
 
+  /**
+   * Deactivate stylesheet by source
+   *
+   * @param current
+   */
   public unset(current: StylesSource) {
     this._styleStateMachine[current].isSet = false
   }
 
+  /**
+   * get fallback style relative to current style
+   *
+   * @param current source of styles
+   * @returns fallback stylesheet
+   */
   public getFallback(current: StylesSource): ContainerStyleSheet {
-    const fallback = this._elementStyles[this._styleStateMachine[current].fallbackStyle]
-    const isSet = this._styleStateMachine[this._styleStateMachine[current].fallbackStyle].isSet
+    const fallbackSource = this._styleStateMachine[current].fallbackStyle
 
-    if (fallback && isSet) {
-      return fallback
+    const fallbackStyles = this._elementStyles[fallbackSource]
+    const isSet = this._styleStateMachine[fallbackSource].isSet
+
+    if (fallbackStyles && isSet) {
+      return fallbackStyles
     }
 
-    return this.getFallback(this._styleStateMachine[current].fallbackStyle)
+    return this.getFallback(fallbackSource)
   }
 }
 
+/**
+ * Stylesheet dependency state machine
+ *
+ * @returns state machine
+ */
 function declareStyleStateMachine() {
   return {
     [StylesSource.focus]: {
@@ -54,6 +75,7 @@ function declareStyleStateMachine() {
     },
     [StylesSource.styleSheet]: {
       fallbackStyle: StylesSource.styleSheet,
+      // it's always activated
       isSet: true
     }
   }
