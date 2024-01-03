448490748d6142cd6970351fe11eaeec7b0d8531
Artem Mangilev <artmnglv@gmail.com>
2024-01-03 15:41:01 +0600

    refactor types

--- projects/ngx-vflow-lib/src/lib/vdoc/interfaces/animation.interface.ts
+++ projects/ngx-vflow-lib/src/lib/vdoc/interfaces/animation.interface.ts
@@ -4,38 +4,40 @@ export type Animation =
   BorderColorAnimation |
   BackgroundColorAnimation
 
-export type AnimationProperty = 'borderRadius' | 'borderWidth' | 'borderColor' | 'backgroundColor'
-
-interface BorderRadiusAnimation {
-  property: 'borderRadius'
-  from: number
-  to: number
-  duration: number
-  animationFunction?: AnimationFunction
-}
-
-interface BorderWidthAnimation {
-  property: 'borderWidth'
-  from: number
-  to: number,
-  duration: number
-  animationFunction?: AnimationFunction
-}
-
-interface BorderColorAnimation {
-  property: 'borderColor'
-  from: string
-  to: string,
-  duration: number
-  animationFunction?: AnimationFunction
-}
-
-interface BackgroundColorAnimation {
-  property: 'backgroundColor'
-  from: string
-  to: string,
-  duration: number
-  animationFunction?: AnimationFunction
-}
+export type AnimationProperty = Animation['property']
 
 export type AnimationFunction = 'linear' | 'ease' | 'ease-in' | 'ease-out' | 'ease-in-out'
+
+interface BorderRadiusAnimation extends WithNumberValues, WithDuration, WithAnimationFunction {
+  property: 'borderRadius'
+}
+
+interface BorderWidthAnimation extends WithNumberValues, WithDuration, WithAnimationFunction {
+  property: 'borderWidth'
+}
+
+interface BorderColorAnimation extends WithStringValues, WithDuration, WithAnimationFunction {
+  property: 'borderColor'
+}
+
+interface BackgroundColorAnimation extends WithStringValues, WithDuration, WithAnimationFunction {
+  property: 'backgroundColor'
+}
+
+interface WithDuration {
+  duration: number
+}
+
+interface WithStringValues {
+  from: string
+  to: string
+}
+
+interface WithNumberValues {
+  from: number
+  to: number
+}
+
+interface WithAnimationFunction {
+  animationFunction?: AnimationFunction
+}
