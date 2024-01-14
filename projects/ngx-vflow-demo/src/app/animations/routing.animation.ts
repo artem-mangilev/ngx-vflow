8be8dcce1880263760be79049cbcbabc485e6645
Artem Mangilev <artmnglv@gmail.com>
2024-01-14 13:16:00 +0600

    add routing animation

---
+++ projects/ngx-vflow-demo/src/app/animations/routing.animation.ts
@@ -0,0 +1,27 @@
+import { animate, animateChild, group, query, style, transition, trigger } from '@angular/animations';
+
+export const routingAnimation = () =>
+  trigger('routingAnimation', [
+    transition('* <=> *', [
+      style({ position: 'relative' }),
+      query(
+        ':enter, :leave',
+        [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+          }),
+        ],
+        { optional: true },
+      ),
+      query(':enter', [style({ opacity: 0 })], { optional: true }),
+      query(':leave', animateChild(), { optional: true }),
+      group([
+        query(':leave', [animate('200ms ease-out', style({ opacity: 0 }))], { optional: true }),
+        query(':enter', [animate('300ms ease-out', style({ opacity: 1 }))], { optional: true }),
+        query('@*', animateChild(), { optional: true }),
+      ]),
+    ]),
+  ])
--- projects/ngx-vflow-demo/src/app/app.component.ts
+++ projects/ngx-vflow-demo/src/app/app.component.ts
@@ -1,12 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { styles } from './app.styles';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { ChildrenOutletContexts } from '@angular/router';
+import { routingAnimation } from './animations/routing.animation';
 
 @Component({
+  animations: [routingAnimation()],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  protected styles = styles;
+  private contexts = inject(ChildrenOutletContexts)
+
+  getRouteAnimationData() {
+    return this.contexts.getContext('primary')?.route?.snapshot?.title
+  }
 }
