22b598a70fd38531ba40128132d0d28206af07b0
Artem Mangilev <artmnglv@gmail.com>
2024-01-18 12:46:57 +0600

    refactor node to separate component & move drag logic to directive

--- projects/ngx-vflow-lib/src/lib/vflow/components/vflow/vflow.component.ts
+++ projects/ngx-vflow-lib/src/lib/vflow/components/vflow/vflow.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, ContentChild, ElementRef, HostListener, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ContentChild, ElementRef, HostListener, Input, OnInit, TemplateRef, ViewChild, inject } from '@angular/core';
 import { Node } from '../../interfaces/node.interface';
 import { Point } from '../../interfaces/point.interface';
+import { DraggableContextDirective } from '../../directives/draggable-context.directive';
 
 @Component({
   selector: 'vflow',
@@ -24,66 +25,22 @@ export class VflowComponent {
   @Input()
   public background: string = '#FFFFFF'
 
-  @ViewChild('flow', { static: true })
-  protected flowRef!: ElementRef<SVGSVGElement>
+  @ViewChild(DraggableContextDirective)
+  protected draggableContext!: DraggableContextDirective
 
   @ContentChild('nodeTemplate')
   protected nodeTemplate!: TemplateRef<any>
 
   protected get flowWidth() {
-    if (this.view === 'auto') {
-      return '100%'
-    }
-
-    return this.view[0]
+    return this.view === 'auto' ? '100%' : this.view[0]
   }
 
   protected get flowHeight() {
-    if (this.view === 'auto') {
-      return '100%'
-    }
-
-    return this.view[1]
+    return this.view === 'auto' ? '100%' : this.view[1]
   }
 
-  private draggingNode: Node | null = null
-  private draggingNodeOffset: Point = { x: 0, y: 0 }
+  protected cdr = inject(ChangeDetectorRef)
 
-  protected onDragStart(node: Node, event: Event) {
-    this.draggingNode = node
-
-    this.draggingNodeOffset = this.getMousePosition(event as MouseEvent)
-    this.draggingNodeOffset.x -= node.position.x;
-    this.draggingNodeOffset.y -= node.position.y;
-  }
-
-  @HostListener('mousemove', ['$event'])
-  protected onDrag(event: Event) {
-    if (this.draggingNode) {
-      event.preventDefault()
-      const position = this.getMousePosition(event as MouseEvent)
-
-      // TODO do not update model's value
-      this.draggingNode.position = {
-        x: position.x - this.draggingNodeOffset.x,
-        y: position.y - this.draggingNodeOffset.y,
-      }
-    }
-  }
-
-  @HostListener('mouseup')
-  protected onDragEnd() {
-    this.draggingNode = null
-    this.draggingNodeOffset = { x: 0, y: 0 }
-  }
-
-  protected getMousePosition(event: MouseEvent) {
-    const CTM = this.flowRef.nativeElement.getScreenCTM()
-
-    return {
-      x: (event.clientX - CTM!.e) / CTM!.a,
-      y: (event.clientY - CTM!.f) / CTM!.d
-    };
-  }
+  protected trackById = (idx: number, node: Node) => node.id
 }
 
--- projects/ngx-vflow-lib/src/lib/vflow/vflow.module.ts
+++ projects/ngx-vflow-lib/src/lib/vflow/vflow.module.ts
@@ -1,15 +1,22 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { VflowComponent } from './components/vflow/vflow.component';
+import { DraggableContextDirective } from './directives/draggable-context.directive';
+import { NodeComponent } from './components/node/node.component';
 
 const components = [
-  VflowComponent
+  VflowComponent,
+  NodeComponent
+]
+
+const directives = [
+  DraggableContextDirective
 ]
 
 @NgModule({
   imports: [CommonModule],
   exports: [VflowComponent],
-  declarations: [...components],
+  declarations: [...components, ...directives],
   providers: [],
 })
 export class VflowModule { }
