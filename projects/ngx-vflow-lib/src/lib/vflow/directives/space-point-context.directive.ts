50388d44658a7d07b082e9b1e4fe5857a96aba3d
Artem Mangilev <artmnglv@gmail.com>
2024-02-10 14:09:24 +0600

    refactor space point context directive

--- projects/ngx-vflow-lib/src/lib/vflow/directives/space-point-context.directive.ts
+++ projects/ngx-vflow-lib/src/lib/vflow/directives/space-point-context.directive.ts
@@ -1,6 +1,6 @@
 import { Directive, ElementRef, Signal, computed, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { fromEvent } from 'rxjs';
+import { fromEvent, map, startWith } from 'rxjs';
 import { RootSvgReferenceDirective } from './reference.directive';
 import { Point } from '../interfaces/point.interface';
 
@@ -12,23 +12,20 @@ export class SpacePointContextDirective {
   public svgCurrentSpacePoint: Signal<Point> = computed(() => {
     const movement = this.mouseMovement()
 
-    if (movement) {
-      const point = this.rootSvgRef.element.createSVGPoint()
-      point.x = movement.clientX
-      point.y = movement.clientY
+    const point = this.rootSvg.createSVGPoint()
+    point.x = movement.x
+    point.y = movement.y
 
-      return point.matrixTransform(
-        this.hostRef.nativeElement.getScreenCTM()!.inverse()
-      );
-    }
-
-    return { x: 0, y: 0 }
+    return point.matrixTransform(this.host.getScreenCTM()!.inverse())
   })
 
-  private rootSvgRef = inject(RootSvgReferenceDirective)
-  private hostRef = inject<ElementRef<SVGGElement>>(ElementRef)
+  private rootSvg = inject(RootSvgReferenceDirective).element
+  private host = inject<ElementRef<SVGGElement>>(ElementRef).nativeElement
 
   private mouseMovement = toSignal(
-    fromEvent<MouseEvent>(this.rootSvgRef.element, 'mousemove')
+    fromEvent<MouseEvent>(this.rootSvg, 'mousemove').pipe(
+      map(event => ({ x: event.clientX, y: event.clientY })),
+    ),
+    { initialValue: { x: 0, y: 0 } }
   )
 }
