a5e4bddd27f8bd02b0693d1c064d8d158c970d00
Artem Mangilev <artmnglv@gmail.com>
2024-04-13 21:03:57 +0500

    docs: add link to repo

---
+++ projects/ngx-vflow-demo/ng-doc.config.ts
@@ -0,0 +1,11 @@
+import { NgDocConfiguration } from '@ng-doc/builder';
+
+const config: NgDocConfiguration = {
+  repoConfig: {
+    url: 'https://github.com/artem-mangilev/ngx-vflow',
+    mainBranch: 'main',
+    releaseBranch: 'main',
+  },
+};
+
+export default config;
