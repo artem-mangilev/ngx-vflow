9e1778d0a7beab5fb78ba64575125b624d8a754d
Artem Mangilev <artmnglv@gmail.com>
2024-05-12 15:10:30 +0500

    feat: do not reset selection on pan

--- projects/ngx-vflow-lib/src/lib/vflow/directives/map-context.directive.ts
+++ projects/ngx-vflow-lib/src/lib/vflow/directives/map-context.directive.ts
@@ -1,14 +1,21 @@
-import { Directive, ElementRef, Injector, Input, OnInit, effect, inject, runInInjectionContext } from '@angular/core';
+import { Directive, ElementRef, Injector, Input, OnInit, effect, inject, runInInjectionContext, untracked } from '@angular/core';
 import { select } from 'd3-selection';
-import { D3ZoomEvent, ZoomBehavior, zoom, zoomIdentity, zoomTransform } from 'd3-zoom';
+import { D3ZoomEvent, ZoomBehavior, ZoomTransform, zoom, zoomIdentity, zoomTransform } from 'd3-zoom';
+import * as d3 from 'd3-selection'
 import { ViewportService } from '../services/viewport.service';
 import { isDefined } from '../utils/is-defined';
 import { RootSvgReferenceDirective } from './reference.directive';
+import { FlowEntitiesService } from '../services/flow-entities.service';
+import { BehaviorSubject, Subject, combineLatest, filter, map, pairwise, switchMap, tap } from 'rxjs';
+import { ViewportState } from '../interfaces/viewport.interface';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
 
 type ZoomEvent = D3ZoomEvent<SVGSVGElement, unknown>
 
 @Directive({ selector: 'g[mapContext]' })
 export class MapContextDirective implements OnInit {
+  private static delta = 6
+
   @Input()
   public minZoom!: number
 
@@ -18,9 +25,59 @@ export class MapContextDirective implements OnInit {
   protected rootSvg = inject(RootSvgReferenceDirective).element
   protected host = inject<ElementRef<SVGGElement>>(ElementRef).nativeElement
   protected viewportService = inject(ViewportService)
+  protected flowEntitiesService = inject(FlowEntitiesService)
 
   protected rootSvgSelection = select(this.rootSvg)
   protected zoomableSelection = select(this.host)
+
+  protected viewportStart$ = new BehaviorSubject<ViewportState>(
+    this.viewportService.readableViewport()
+  )
+  protected viewportEnd$ = new BehaviorSubject<ViewportState>(
+    this.viewportService.readableViewport()
+  )
+
+  protected panStart = toSignal(
+    this.viewportStart$.pipe(
+      pairwise(),
+      filter(([prevState, currentState]) => {
+        // pass only pan changes
+        return prevState.zoom === currentState.zoom
+      }),
+      map(([, currentState]) => currentState),
+    )
+  )
+
+  protected panEnd = toSignal(
+    this.viewportEnd$.pipe(
+      pairwise(),
+      filter(([prevState, currentState]) => {
+        // pass only pan changes
+        return prevState.zoom === currentState.zoom
+      }),
+      map(([, currentState]) => currentState),
+    )
+  )
+
+  protected resetSelection = effect(() => {
+    const panStart = untracked(this.panStart)
+    const panEnd = this.panEnd()
+
+    if (panStart && panEnd) {
+      const delta = MapContextDirective.delta
+
+      const diffX = Math.abs(panEnd.x - panStart.x)
+      const diffY = Math.abs(panEnd.y - panStart.y)
+
+      // click (not drag)
+      if (diffX < delta && diffY < delta) {
+        this.flowEntitiesService.select(null)
+      }
+    }
+
+    // TODO: allowSignalWrites
+  }, { allowSignalWrites: true })
+
 
   // under the hood this effect triggers handleZoom, so error throws without this flag
   // TODO: hack with timer fixes wrong node scaling (handle positions not matched with content size)
@@ -61,15 +118,27 @@ export class MapContextDirective implements OnInit {
   public ngOnInit(): void {
     this.zoomBehavior = zoom<SVGSVGElement, unknown>()
       .scaleExtent([this.minZoom, this.maxZoom])
+      .on('start', (event: ZoomEvent) => this.onD3zoomStart(event))
       .on('zoom', this.handleZoom)
+      .on('end', (event: ZoomEvent) => this.onD3zoomEnd(event))
 
     this.rootSvgSelection.call(this.zoomBehavior)
   }
 
   private handleZoom = ({ transform }: ZoomEvent) => {
     // update public signal for user to read
-    this.viewportService.readableViewport.set({ zoom: transform.k, x: transform.x, y: transform.y })
+    this.viewportService.readableViewport.set(mapTransformToViewportState(transform))
 
     this.zoomableSelection.attr('transform', transform.toString())
   }
+
+  private onD3zoomStart({ transform }: ZoomEvent) {
+    this.viewportStart$.next(mapTransformToViewportState(transform))
+  }
+
+  private onD3zoomEnd({ transform }: ZoomEvent) {
+    this.viewportEnd$.next(mapTransformToViewportState(transform))
+  }
 }
+
+const mapTransformToViewportState = (transform: ZoomTransform): ViewportState => ({ zoom: transform.k, x: transform.x, y: transform.y })
--- projects/ngx-vflow-lib/src/lib/vflow/directives/root-svg-context.directive.ts
+++ projects/ngx-vflow-lib/src/lib/vflow/directives/root-svg-context.directive.ts
@@ -1,12 +1,11 @@
 import { Directive, HostListener, inject } from '@angular/core';
 import { FlowStatusService } from '../services/flow-status.service';
-import { FlowEntitiesService } from '../services/flow-entities.service';
+
 
 // TODO: too general purpose nane
 @Directive({ selector: 'svg[rootSvgContext]' })
 export class RootSvgContextDirective {
   private flowStatusService = inject(FlowStatusService)
-  private flowEntitiesService = inject(FlowEntitiesService)
 
   // TODO: check for multiple instances on page
   @HostListener('document:mouseup')
@@ -16,16 +15,5 @@ export class RootSvgContextDirective {
     if (status.state === 'connection-start') {
       this.flowStatusService.setIdleStatus()
     }
-  }
-
-  @HostListener('mousedown', ['$event'])
-  protected resetSelection(event: PointerEvent) {
-    // ignore reset if chain conntains selectable element
-    // in order to not undo its selection
-    if ((event.target as Element).closest('.selectable')) {
-      return
-    }
-
-    this.flowEntitiesService.select(null)
   }
 }
