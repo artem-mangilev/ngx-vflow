a1103b1aaa75d8245307fb9c0623f78e9cc5ca47
Artem Mangilev <artmnglv@gmail.com>
2024-06-23 17:44:52 +0500

    docs: extend doc with component nodes description

---
+++ projects/ngx-vflow-demo/src/app/categories/examples/pages/custom-nodes/demo/custom-component-nodes-demo.component.ts
@@ -0,0 +1,130 @@
+import { NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnInit, Output, inject } from '@angular/core';
+import { HotToastService } from '@ngneat/hot-toast';
+import { ComponentNodeEvent, CustomNodeComponent, Edge, Node, VflowModule } from 'projects/ngx-vflow-lib/src/public-api';
+
+@Component({
+  template: `<vflow [nodes]="nodes" [edges]="edges" (onComponentNodeEvent)="handleComponentEvent($event)" />`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  standalone: true,
+  imports: [VflowModule]
+})
+export class CustomComponentNodesDemoComponent {
+  private toast = inject(HotToastService);
+
+  public nodes: Node[] = [
+    {
+      id: '1',
+      point: { x: 100, y: 100 },
+      type: RedSquareNodeComponent,
+      data: {
+        redSquareText: 'Red',
+      } satisfies RedSquareData
+    },
+    {
+      id: '2',
+      point: { x: 250, y: 250 },
+      type: BlueSquareNodeComponent,
+      data: {
+        blueSquareText: 'Blue',
+      } satisfies BlueSquareData
+    },
+  ]
+
+  public edges: Edge[] = [
+    {
+      id: '1 -> 2',
+      source: '1',
+      target: '2'
+    }
+  ]
+
+  // Type-safe!
+  handleComponentEvent(event: ComponentNodeEvent<[RedSquareNodeComponent, BlueSquareNodeComponent]>) {
+    if (event.eventName === 'redSquareEvent') {
+      this.toast.info(event.eventPayload)
+    }
+
+    if (event.eventName === 'blueSquareEvent') {
+      this.toast.info(`${event.eventPayload.x + event.eventPayload.y}`)
+    }
+  }
+}
+
+// --- Description of red square component node
+
+interface RedSquareData {
+  redSquareText: string
+}
+
+@Component({
+  template: `
+    <div class="red-square" (click)="onClick()">
+      {{ node.data?.redSquareText }}
+
+      <handle type="source" position="right"/>
+    </div>
+  `,
+  styles: [`
+    .red-square {
+      width: 100px;
+      height: 100px;
+      background-color: #DE3163;
+      border-radius: 5px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding-left: 5px;
+      padding-right: 5px;
+    }
+  `],
+  standalone: true,
+  imports: [VflowModule]
+})
+export class RedSquareNodeComponent extends CustomNodeComponent<RedSquareData> {
+  @Output()
+  redSquareEvent = new EventEmitter<string>()
+
+  onClick() {
+    this.redSquareEvent.emit('Click from red square')
+  }
+}
+
+// --- Description of blue square component node
+
+interface BlueSquareData {
+  blueSquareText: string
+}
+
+@Component({
+  template: `
+    <div class="blue-square" (click)="onClick()">
+      {{ node.data?.blueSquareText }}
+
+      <handle type="target" position="left"/>
+    </div>
+  `,
+  styles: [`
+    .blue-square {
+      width: 100px;
+      height: 100px;
+      background-color: #0096FF;
+      border-radius: 5px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      padding-left: 5px;
+      padding-right: 5px;
+    }
+  `],
+  standalone: true,
+  imports: [VflowModule]
+})
+export class BlueSquareNodeComponent extends CustomNodeComponent<BlueSquareData> {
+  @Output()
+  blueSquareEvent = new EventEmitter<{ x: number, y: number }>()
+
+  onClick() {
+    this.blueSquareEvent.emit({ x: 5, y: 5 })
+  }
+}
--- projects/ngx-vflow-demo/src/app/categories/examples/pages/custom-nodes/index.md
+++ projects/ngx-vflow-demo/src/app/categories/examples/pages/custom-nodes/index.md
@@ -5,6 +5,10 @@
 
 This is where things became a lot more interesting. You can create custom nodes with HTML and CSS.
 
+## Template nodes
+
+You can create nodes custom nodes with `ng-template`
+
 Do the following steps to archieve this:
 
 1. Set `type` of node to `html-template`
@@ -13,3 +17,63 @@ Do the following steps to archieve this:
 4. You can also pass any data with `data` field on node, and then get it inside `ng-template`
 
 {{ NgDocActions.demo("CustomNodesDemoComponent", { expanded: true }) }}
+
+## Component nodes
+
+Another approach is to render nodes from components.
+
+It's benefits:
+
+- type-safe node data access
+- good for complex flows with a lot of different node types
+
+It's limitations
+
+- it's harder to manage events because such nodes rendered dynamically
+
+How to create component node:
+
+1. Create a regular angular standalone component
+2. Extend with `CustomNodeComponent`, otherwise it won't work!
+3. Pass your data interface to generic of `CustomNodeComponent` to use in component. This `data` comes from `Node` definition
+4. Use your new component in `type` field of `Node`. Library will render your node for you
+
+{{ NgDocActions.demo("CustomComponentNodesDemoComponent", { expanded: true }) }}
+
+### Handling events
+
+> **Warning**
+> This is an experimental API
+
+There is a `(onComponentNodeEvent)` event on `VflowComponent`, how it works
+
+1. It accumulates every `EventEmitter` of every component node of your flow
+2. It emits on every emit of those emitters
+
+The shape of this accumulator-event contains following useful info:
+
+```ts
+export type AnyComponentNodeEvent = {
+  nodeId: string // Id of node where event occurs
+  eventName: string
+  eventPayload: unknown
+}
+```
+
+The Library also includes `ComponentNodeEvent` helper type to get type-safe event, where you just need to pass an array of your custom components in generic, and this type will infer proper types for `eventName` and `eventPayload`:
+
+```ts
+  ...
+
+  handleComponentEvent(event: ComponentNodeEvent<[RedSquareNodeComponent, BlueSquareNodeComponent]>) {
+    if (event.eventName === 'redSquareEvent') {
+      console.log(event.eventPayload)
+    }
+
+    if (event.eventName === 'blueSquareEvent') {
+      console.log(event.eventPayload.x + event.eventPayload.y)
+    }
+  }
+
+  ..
+```
