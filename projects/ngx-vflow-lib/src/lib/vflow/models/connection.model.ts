c114ef153d40e2f3004363641d132b3d505f8fc1
Artem Mangilev <artmnglv@gmail.com>
2024-07-06 13:46:30 +0500

    feat: create coonection mode option

--- projects/ngx-vflow-lib/src/lib/vflow/models/connection.model.ts
+++ projects/ngx-vflow-lib/src/lib/vflow/models/connection.model.ts
@@ -1,14 +1,17 @@
 import { ConnectionSettings, ConnectionValidatorFn } from "../interfaces/connection-settings.interface";
 import { Curve, EdgeType } from "../interfaces/edge.interface";
+import { ConnectionMode } from "../types/connection-mode.type";
 
 export class ConnectionModel {
   public curve: Curve
   public type: EdgeType
   public validator: ConnectionValidatorFn
+  public mode: ConnectionMode
 
   constructor(public connection: ConnectionSettings) {
     this.curve = connection.curve ?? 'bezier'
     this.type = connection.type ?? 'default'
     this.validator = connection.validator ?? (() => true)
+    this.mode = connection.mode ?? 'strict'
   }
 }
---
+++ projects/ngx-vflow-lib/src/lib/vflow/types/connection-mode.type.ts
@@ -0,0 +1,1 @@
+export type ConnectionMode = 'loose' | 'strict'
