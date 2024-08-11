970223592cc0e2248aa3f7def0a14acc2fbb6dbe
Artem Mangilev <artmnglv@gmail.com>
2024-08-11 17:28:08 +0500

    docs: add page for dynamic nodes

---
+++ projects/ngx-vflow-demo/src/app/categories/features/pages/dynamic-vs-regular-nodes/ng-doc.page.ts
@@ -0,0 +1,11 @@
+import { NgDocPage } from '@ng-doc/core';
+import ExamplesCategory from '../../ng-doc.category'
+
+const TestPage: NgDocPage = {
+  title: `Dynamic vs Regular nodes`,
+  mdFile: './index.md',
+  category: ExamplesCategory,
+  order: 2
+};
+
+export default TestPage;
