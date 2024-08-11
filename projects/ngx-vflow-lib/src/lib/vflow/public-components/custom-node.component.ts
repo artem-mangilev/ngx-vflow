76a907700ed047dc79f8189c9d8218a424647423
Artem Mangilev <artmnglv@gmail.com>
2024-08-11 16:41:39 +0500

    refactor: move shared logic for default and dynamic node

--- projects/ngx-vflow-lib/src/lib/vflow/public-components/custom-node.component.ts
+++ projects/ngx-vflow-lib/src/lib/vflow/public-components/custom-node.component.ts
@@ -1,69 +1,21 @@
-import { DestroyRef, Directive, EventEmitter, Input, OnInit, inject, signal } from "@angular/core"
+import { Directive, Input, OnInit } from "@angular/core"
 import { ComponentNode } from '../interfaces/node.interface';
-import { ComponentEventBusService } from "../services/component-event-bus.service";
-import { merge, tap } from "rxjs";
-import { takeUntilDestroyed } from "@angular/core/rxjs-interop";
+import { CustomNodeBaseComponent } from "../components/custom-node-base/custom-node-base.component";
 
 @Directive()
-export abstract class CustomNodeComponent<T = unknown> implements OnInit {
-  private eventBus = inject(ComponentEventBusService)
-
-  protected destroyRef = inject(DestroyRef)
-
+export abstract class CustomNodeComponent<T = unknown> extends CustomNodeBaseComponent<T> implements OnInit {
   /**
    * Reference to node bound to this component
    */
   @Input()
-  public node!: ComponentNode<T>
+  public override node!: ComponentNode<T>
 
-  @Input()
-  public set _selected(value: boolean) {
-    this.selected.set(value)
-  }
-
-  /**
-   * Signal with selected state of node
-   */
-  public selected = signal(false)
-
-  public data = signal<T | undefined>(undefined)
-
-  public ngOnInit(): void {
+  public override ngOnInit(): void {
     if (this.node.data) {
       this.data.set(this.node.data)
     }
 
-    this.trackEvents()
+    super.ngOnInit()
   }
-
-  private trackEvents() {
-    const props = Object.getOwnPropertyNames(this)
-
-    const emitters = new Map<EventEmitter<unknown>, string>()
-    for (const prop of props) {
-      const field = (this as Record<string, unknown>)[prop]
-
-      if (field instanceof EventEmitter) {
-        emitters.set(field, prop)
-      }
-    }
-
-    merge(
-      ...Array.from(emitters.keys()).map(emitter =>
-        emitter.pipe(
-          tap((event) => {
-            this.eventBus.pushEvent({
-              nodeId: this.node.id,
-              eventName: emitters.get(emitter)!,
-              eventPayload: event
-            })
-          }))
-      )
-    )
-      .pipe(
-        takeUntilDestroyed(this.destroyRef)
-      )
-      .subscribe()
-  };
 }
 
