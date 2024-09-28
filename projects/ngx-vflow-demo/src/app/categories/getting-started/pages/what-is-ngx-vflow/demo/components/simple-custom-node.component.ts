92ddef00aae7b06d8ea357da504162fd265cc4e2
Artem Mangilev <artmnglv@gmail.com>
2024-09-28 19:40:17 +0500

    docs: separate components for all features demo

---
+++ projects/ngx-vflow-demo/src/app/categories/getting-started/pages/what-is-ngx-vflow/demo/components/simple-custom-node.component.ts
@@ -0,0 +1,27 @@
+import { Component } from "@angular/core";
+import { CustomNodeComponent, VflowModule } from "projects/ngx-vflow-lib/src/public-api";
+
+@Component({
+  template: `<div class="node">
+    Custom node!
+
+    <handle type="target" position="top" />
+    <handle type="source" position="bottom" />
+  </div>`,
+  styles: [`
+    .node {
+      width: 150px;
+      height: 100px;
+      background: #bbe1fa;
+      border: 1px solid gray;
+      border-radius: 5px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: black;
+    }
+  `],
+  standalone: true,
+  imports: [VflowModule]
+})
+export class SimpleCustomNodeComponent extends CustomNodeComponent { }
