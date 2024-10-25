60b85dcfddf4530741c32f7d98bd18a1096edd24
Artem Mangilev <artmnglv@gmail.com>
2024-10-25 16:45:54 +0500

    feat: adjust minimap viewport based on distance between nodes

--- projects/ngx-vflow-lib/src/lib/vflow/public-components/minimap/minimap.component.ts
+++ projects/ngx-vflow-lib/src/lib/vflow/public-components/minimap/minimap.component.ts
@@ -3,6 +3,10 @@ import { FlowEntitiesService } from '../../services/flow-entities.service';
 import { MinimapModel } from '../../models/minimap.model';
 import { NodeModel } from '../../models/node.model';
 import { id } from '../../utils/id';
+import { FlowSettingsService } from '../../services/flow-settings.service';
+import { getViewportForBounds } from '../../utils/viewport';
+import { getNodesBounds } from '../../utils/nodes';
+import { ViewportService } from '../../services/viewport.service';
 
 @Component({
   selector: 'minimap',
@@ -11,12 +15,31 @@ import { id } from '../../utils/id';
 })
 export class MinimapComponent implements OnInit {
   protected entitiesService = inject(FlowEntitiesService)
+  protected flowSettingsService = inject(FlowSettingsService)
 
   @ViewChild('minimap', { static: true })
   private minimap!: TemplateRef<unknown>
 
   protected clipPathId = signal(id())
   protected clipPathUrl = computed(() => `url(#${this.clipPathId()})`)
+
+  protected minimapWidth = computed(() => this.flowSettingsService.computedFlowWidth() * 0.2)
+  protected minimapHeight = computed(() => this.flowSettingsService.computedFlowHeight() * 0.2)
+
+  protected minimapTransform = computed(() => {
+    const nodes = this.entitiesService.nodes()
+
+    const state = getViewportForBounds(
+      getNodesBounds(nodes),
+      this.flowSettingsService.computedFlowWidth(),
+      this.flowSettingsService.computedFlowHeight(),
+      this.flowSettingsService.minZoom(),
+      this.flowSettingsService.maxZoom(),
+      0
+    )
+
+    return `translate(${state.x * 0.2}, ${state.y * 0.2}) scale(${state.zoom * 0.2})`
+  })
 
   public ngOnInit(): void {
     const model = new MinimapModel()
