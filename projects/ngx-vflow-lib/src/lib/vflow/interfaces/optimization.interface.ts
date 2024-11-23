438d8124a4a670e67d7516a5669770744dca7a5c
Artem Mangilev <artmnglv@gmail.com>
2024-11-23 16:53:58 +0500

    feat: add description for optimization fields

--- projects/ngx-vflow-lib/src/lib/vflow/interfaces/optimization.interface.ts
+++ projects/ngx-vflow-lib/src/lib/vflow/interfaces/optimization.interface.ts
@@ -1,4 +1,16 @@
 export interface Optimization {
+  /**
+   * If true, compute the right nodes order by z-axis inside groups on initial render.
+   *
+   * @default true
+   */
   computeLayersOnInit: boolean
+
+  /**
+   * If true, the layer with groups will be placed behind the edges layer.
+   * This approach fixes the issue when you can't select an edge inside group.
+   *
+   * @default false
+   */
   detachedGroupsLayer: boolean
 }
