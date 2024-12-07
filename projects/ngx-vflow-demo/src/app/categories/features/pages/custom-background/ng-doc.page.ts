68bf1b2d3744077e00d4150d1c255a6e86767f3a
Artem Mangilev <artmnglv@gmail.com>
2024-12-07 15:27:11 +0500

    docs: add section about image bg

--- projects/ngx-vflow-demo/src/app/categories/features/pages/custom-background/ng-doc.page.ts
+++ projects/ngx-vflow-demo/src/app/categories/features/pages/custom-background/ng-doc.page.ts
@@ -2,12 +2,17 @@ import { NgDocPage } from '@ng-doc/core';
 import ExamplesCategory from '../../ng-doc.category'
 import { CustomBackgroundDemoComponent } from './demo/custom-background-demo.component';
 import { DotsCustomBackgroundDemoComponent } from './demo/dots-custom-background-demo.component';
+import { ImageCustomBackgroundDemoComponent } from './demo/image-custom-background-demo.component';
 
 const TestPage: NgDocPage = {
   title: `Custom background`,
   mdFile: './index.md',
   category: ExamplesCategory,
-  demos: { CustomBackgroundDemoComponent, DotsCustomBackgroundDemoComponent },
+  demos: {
+    CustomBackgroundDemoComponent,
+    DotsCustomBackgroundDemoComponent,
+    ImageCustomBackgroundDemoComponent
+  },
   order: 11
 };
 
