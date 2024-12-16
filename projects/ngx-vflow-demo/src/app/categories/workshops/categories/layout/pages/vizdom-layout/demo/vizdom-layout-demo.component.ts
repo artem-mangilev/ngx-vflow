40d4699f9f1caa5cc660081c6854fb8ef05e3ae0
Artem Mangilev <artmnglv@gmail.com>
2024-12-16 14:45:02 +0500

    feat: use step curve for vizdom layout

--- projects/ngx-vflow-demo/src/app/categories/workshops/categories/layout/pages/vizdom-layout/demo/vizdom-layout-demo.component.ts
+++ projects/ngx-vflow-demo/src/app/categories/workshops/categories/layout/pages/vizdom-layout/demo/vizdom-layout-demo.component.ts
@@ -72,6 +72,7 @@ export class VizdomLayoutDemoComponent implements OnInit {
         source: node.id,
         target: newNodeId,
         id: `${node.id} -> ${newNodeId}`,
+        curve: 'smooth-step'
       },
     ];
 
