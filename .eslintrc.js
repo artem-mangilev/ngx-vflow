32273f3664c9bbc799f1a23477f23439f25ae722
Artem Mangilev <artmnglv@gmail.com>
2025-01-04 14:09:23 +0500

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  overrides: [
+    {
+      files: ["*.ts"],
+      parserOptions: {
+        project: [
+          "tsconfig.*?.json",
+          // "e2e/tsconfig.json"
+        ],
+        createDefaultProgram: true
+      },
+      extends: ["plugin:@angular-eslint/recommended"],
+    },
+    {
+      files: ["*.component.html"],
+      extends: ["plugin:@angular-eslint/template/recommended"],
+      rules: {
+        "max-len": ["error", { "code": 140 }]
+      }
+    },
+    {
+      files: ["*.component.ts"],
+      extends: ["plugin:@angular-eslint/template/process-inline-templates"]
+    }
+  ]
+}
