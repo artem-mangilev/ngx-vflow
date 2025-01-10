0cd27ca9a9a31738f4da273165bf148dbba40aa3
Artem Mangilev <artmnglv@gmail.com>
2025-01-10 18:05:06 +0500

    test: add test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('has logo text', async ({ page }) => {
+  await page.goto('http://localhost:4200');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByTestId('logo-text')).toHaveText('ngx-vflow');
 });
