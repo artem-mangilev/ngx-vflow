0000ec60fbf5dc5f7d26f483403eebb3657103fd
Artem Mangilev <artmnglv@gmail.com>
2025-06-06 17:40:39 +0500

    docs: add comments for factory

--- projects/ngx-vflow-lib/src/lib/vflow/interfaces/curve-factory.interface.ts
+++ projects/ngx-vflow-lib/src/lib/vflow/interfaces/curve-factory.interface.ts
@@ -5,25 +5,41 @@ import { DynamicNode, Node } from './node.interface';
 import { Point } from './point.interface';
 
 export interface CurveFactorySharedParams {
+  /** Starting point coordinates of the curve */
   sourcePoint: Point;
+  /** Ending point coordinates of the curve */
   targetPoint: Point;
+  /** Position of the source handle relative to the source node */
   sourcePosition: Position;
+  /** Position of the target handle relative to the target node */
   targetPosition: Position;
+  /** Array of all edges in the flow */
   allEdges: Edge[];
+  /** Array of all nodes in the flow */
   allNodes: Node[] | DynamicNode[];
 }
 
 export interface ConnectionCurveFactoryParams extends CurveFactorySharedParams {
+  /** Indicates this is a temporary connection being drawn */
   mode: 'connection';
 }
 
 export interface EdgeCurveFactoryParams extends CurveFactorySharedParams {
+  /** Indicates this is a permanent edge */
   mode: 'edge';
+  /** The edge instance this curve belongs to */
   edge: Edge;
 }
 
+/**
+ * Layout information for a curve
+ * */
 export interface CurveLayout {
+  /** SVG path string defining the curve
+   * (d attribute - https://developer.mozilla.org/en-US/docs/Web/SVG/Reference/Attribute/d)
+   * */
   path: string;
+  /** Optional points for label placement along the curve */
   labelPoints?: { [key in EdgeLabelPosition]: Point };
 }
 
