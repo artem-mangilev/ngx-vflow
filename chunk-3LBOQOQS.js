bf86ff4c22d2aa7fb014b3c5b0dffcf2370d4621
Artem Mangilev <artmnglv@gmail.com>
2025-10-26 12:58:43 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/artem-mangilev/ngx-vflow/commit/f66ae28621299ae5f243cc983de84dae40690566

---
+++ chunk-3LBOQOQS.js
@@ -0,0 +1,1 @@
+import"./chunk-ODLL2QMY.js";var t=[{path:"",redirectTo:"force",pathMatch:"full"},{path:"",title:"Layout",children:[{path:"force",loadChildren:()=>import("./chunk-2IIB3JDY.js")},{path:"vizdom-layout",loadChildren:()=>import("./chunk-BJWZLWVY.js")}]}],o=t;export{o as default};
