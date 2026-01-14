63738cd770527ba5b81f4aecbe3acb3eb1c61b26
borisnliscool <boris@borisnl.nl>
2026-01-14 20:44:05 +0100

    feat: add a grid pattern background

--- projects/ngx-vflow-demo/src/app/categories/viewport/pages/custom-background/ng-doc.page.ts
+++ projects/ngx-vflow-demo/src/app/categories/viewport/pages/custom-background/ng-doc.page.ts
@@ -2,6 +2,7 @@ import { NgDocPage } from '@ng-doc/core';
 import ViewportCategory from '../../ng-doc.category';
 import { CustomBackgroundDemoComponent } from './demo/custom-background-demo.component';
 import { DotsCustomBackgroundDemoComponent } from './demo/dots-custom-background-demo.component';
+import { GridCustomBackgroundDemoComponent } from './demo/grid-custom-background-demo.component copy';
 import { ImageCustomBackgroundDemoComponent } from './demo/image-custom-background-demo.component';
 
 const TestPage: NgDocPage = {
@@ -12,6 +13,7 @@ const TestPage: NgDocPage = {
     CustomBackgroundDemoComponent,
     DotsCustomBackgroundDemoComponent,
     ImageCustomBackgroundDemoComponent,
+    GridCustomBackgroundDemoComponent,
   },
   order: 1,
 };
